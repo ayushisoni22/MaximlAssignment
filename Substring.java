643079da0e71e52d18b559e32057f3141a8d0dd6
ayushisoni22 <49267470+ayushisoni22@users.noreply.github.com>
2019-08-07 23:01:27 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Substring
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    String S = sc.nextLine();
+	    String result ="";
+	    
+	    for(int i=0;i<S.length();i++){
+	       if(result.indexOf(S.charAt(i))==-1){
+	           result += S.charAt(i);
+	       }
+	    }
+	    System.out.println(result.length());
+	}
+}
